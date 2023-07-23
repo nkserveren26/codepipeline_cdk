9736b83d23f779ff618640220cd254af92d3cd8b
nkserveren26 <norinoritoshi@outlook.jp>
2023-07-23 15:08:06 +0900

    commit

--- lib/codepipeline_cdk-stack.ts
+++ lib/codepipeline_cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { PipelineCreator } from './services/codepipeline/creator';
+import { IAMCreator } from './services/iam/creator';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CodepipelineCdkStack extends cdk.Stack {
@@ -8,6 +9,9 @@ export class CodepipelineCdkStack extends cdk.Stack {
     super(scope, id, props);
 
     //各ステージの定義
+
+    //CodeBuildプロジェクトで使用するIAMロールを作成
+    const codeBuildRole = IAMCreator.createCodeBuildRole(scope, "codebuild_backend_deploy_role");
     //Pipeline作成
     const pipeline = PipelineCreator.createPipeline(scope, "backend_pipeline");
     //Pipelineにステージを追加
