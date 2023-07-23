de0bab61202b39108f7eeddb1b51434bdb90a880
nkserveren26 <norinoritoshi@outlook.jp>
2023-07-23 15:36:54 +0900

    commit

--- lib/services/codepipeline/creator.ts
+++ lib/services/codepipeline/creator.ts
@@ -1,5 +1,6 @@
 import { IRepository, Repository } from "aws-cdk-lib/aws-codecommit";
-import { Pipeline } from "aws-cdk-lib/aws-codepipeline";
+import { Artifact, Pipeline } from "aws-cdk-lib/aws-codepipeline";
+import * as actions from 'aws-cdk-lib/aws-codepipeline-actions';
 import { Construct } from "constructs";
 
 export class PipelineCreator {
@@ -17,5 +18,15 @@ export class PipelineCreator {
         const repository = Repository.fromRepositoryName(self, repositoryName, repositoryName);
         return repository;
 
+    }
+
+    public static createCodeCommitSourceAction(actionName: string, repo: IRepository, branch: string, sourceOutput: Artifact) {
+        const codeCommitSourceAction = new actions.CodeCommitSourceAction({
+            actionName: actionName,
+            repository: repo,
+            branch: branch,
+            output: sourceOutput,
+        });
+        return codeCommitSourceAction;
     }
 }
