999d8f76694b8c260bd7e0142f57a53d57023c23
nkserveren26 <norinoritoshi@outlook.jp>
2023-07-23 15:21:50 +0900

    commit

--- lib/services/codepipeline/creator.ts
+++ lib/services/codepipeline/creator.ts
@@ -1,14 +1,21 @@
+import { IRepository, Repository } from "aws-cdk-lib/aws-codecommit";
 import { Pipeline } from "aws-cdk-lib/aws-codepipeline";
 import { Construct } from "constructs";
 
 export class PipelineCreator {
-    public static createPipeline(self: Construct,pipelineName: string): Pipeline  {
+    public static createPipeline(self: Construct, pipelineName: string): Pipeline  {
         const pipeline: Pipeline = new Pipeline(self, pipelineName, {
             pipelineName: pipelineName,
         });
         return pipeline;
     }
     public static addStageToPipeline() {
+
+    }
+
+    public static getCodeCommitRepository(self: Construct, repositoryName: string): IRepository {
+        const repository = Repository.fromRepositoryName(self, repositoryName, repositoryName);
+        return repository;
 
     }
 }
